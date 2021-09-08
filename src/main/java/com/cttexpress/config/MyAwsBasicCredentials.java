0bb20183d58a251146d4ad9f35a6fbe56391626b
Pablo Sancho-Sopranis <psanchosopranis@gmail.com>
2021-09-08 10:39:56 +0200

    Anular Credenciales

--- src/main/java/com/cttexpress/config/MyAwsBasicCredentials.java
+++ src/main/java/com/cttexpress/config/MyAwsBasicCredentials.java
@@ -2,7 +2,7 @@ package com.cttexpress.config;
 
 public abstract class MyAwsBasicCredentials {
 
-    public static final String ACCESS_KEY_ID = "AKIA35DMEUA6FQ3MKV4U";
-    public static final String SECRET_ACCESS_KEY = "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";
+    public static final String ACCESS_KEY_ID = "********************************";
+    public static final String SECRET_ACCESS_KEY = "*****************************************";
 
 }
